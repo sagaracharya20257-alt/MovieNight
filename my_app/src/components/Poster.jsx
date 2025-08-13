0a31f884ece94d87e27c32eacd56bb6c13d33260
sagaracharya20257-alt <sagaracharya2025.7@gmail.com>
2025-08-13 13:02:30 +0530

    removed contents of index.css file

--- my_app/src/components/Poster.jsx
+++ my_app/src/components/Poster.jsx
@@ -5,6 +5,5 @@ export default function Poster({ src, alt = "", size = "medium", className = "" 
   const sizeClass =
     size === "small" ? "poster-small" : size === "large" ? "poster-large" : "poster-medium";
 
-  // img-fluid ensures responsive images with Bootstrap
   return <img src={src} alt={alt} className={`${sizeClass} ${className} img-fluid`} />;
 }
